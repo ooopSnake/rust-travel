3f2ecf1e570ff4b2195f46de6d20371d4389a2c8
suzhen <suzhen@homeinpocket.net>
2020-07-07 13:21:51 +0800

    :tada: initial commit

---
+++ tests/test_dst.rs
@@ -0,0 +1,67 @@
+use std::mem;
+
+trait Bird {
+    fn fly(&self);
+    fn beep(&self);
+}
+
+struct Duck;
+
+struct Swan;
+
+impl Bird for Duck {
+    fn fly(&self) {
+        println!("duck duck");
+    }
+
+    fn beep(&self) {
+        println!("beep! duck")
+    }
+}
+
+impl Bird for Swan {
+    fn fly(&self) {
+        println!("swan swan")
+    }
+
+    fn beep(&self) {
+        println!("beep swan")
+    }
+}
+
+/// 参数是 trait object 类型，p 是一个胖指针
+///  `*const ()` 可以视为C/C++中的void*
+fn print_trait_object(p: &dyn Bird) {
+
+    // 使用transmute执行强制类型转换，把变量p的内部数据取出来
+    let (data, vtable): (usize, usize) = unsafe { mem::transmute(p) };
+    println!("TraitObject    [data:0x{:x}, vtable:0x{:x}]", data, vtable);
+    unsafe {
+        // 使用as执行强制类型转换，将vtable从 `usize` 类型转为 `*const usize` 类型
+        let v: *const usize = vtable as *const () as *const usize;
+        // 打印出指针 v 指向的内存区间的值
+        println!("data in vtable [0x{:x}, 0x{:x}, 0x{:x}, 0x{:x}]",
+                 *v, *v.offset(1), *v.offset(2), *v.offset(3));
+    }
+}
+
+#[test]
+fn test_dst() {
+    let duck = Duck;
+    let p_duck = &duck;
+    println!("size of Sized p_duck:{}", mem::size_of_val(&p_duck));
+    // fat pointer
+    let p_bird = p_duck as &dyn Bird;
+    println!("size of ?Size dyn p_bird:{}", mem::size_of_val(&p_bird));
+    // 获取函数地址
+    let duck_fly: usize = Duck::fly as *const () as usize;
+    // 获取函数地址
+    let swan_fly: usize = Swan::fly as *const () as usize;
+    //
+    println!("Duck::fly 0x{:x}", duck_fly);
+    println!("Swan::fly 0x{:x}", swan_fly);
+    //
+    print_trait_object(p_bird);
+    let swan = Swan;
+    print_trait_object(&swan as &dyn Bird);
+}
---
+++ tests/test_macro.rs
@@ -0,0 +1,16 @@
+macro_rules! to_unit_ref {
+    ($($x:tt)*) =>(&());
+}
+
+macro_rules! const_count {
+    ($($i:expr),*) => ({
+     const _V :usize = <[&()]>::len(&[$(to_unit_ref!($i)),*]);
+     _V
+    } );
+}
+
+#[test]
+fn test_macro1(){
+    const CNT: usize = const_count![1,2,3,4,5,6];
+    println!("cnt={}", CNT);
+}

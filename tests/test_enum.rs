1f8c5a3b45440c17c5ce07aad9df827c39fb68c3
suzhen <suzhen@homeinpocket.net>
2020-07-09 11:19:32 +0800

    :sparkles: add test enum

---
+++ tests/test_enum.rs
@@ -0,0 +1,29 @@
+use crate::Wrapper::Empty;
+
+#[derive(Debug, PartialEq)]
+enum Wrapper<T> {
+    Value(T),
+    Empty,
+}
+
+impl<T> Wrapper<T> {
+    fn clear(&mut self) {
+        match self {
+            Wrapper::Value(v) => {
+                *self = Empty
+            }
+            _ => {}
+        }
+    }
+}
+
+#[test]
+fn test_enum() {
+    let x = &1;
+    let wref = &mut Wrapper::Value(x);
+    if let Wrapper::Value(v) = wref {
+        println!("vv:{}", *v)
+    }
+    wref.clear();
+    assert_eq!(*wref, Wrapper::Empty);
+}

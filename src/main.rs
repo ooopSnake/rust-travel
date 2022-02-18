39df63fb3b55f7c7ccf1f2afd4b625eb6831fb62
suzhen <suzhen@guestin.cn>
2022-02-18 16:49:40 +0800

    add mod

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,13 @@ mod test_dst;
 mod test_enum;
 mod test_fn_shadow;
 mod test_lifecycle;
-mod test_macro;
+#[macro_use]
+pub(crate) mod test_macro;
 mod test_rc;
 mod test_unsafe_cell;
 mod test_async_executor;
 mod test_custom_vec;
+mod test_actix_web_route;
 
 fn main() {
     println!("nothing is true,everything is permitted")
